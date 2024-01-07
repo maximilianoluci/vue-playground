d6251a1479deb6757715f623da5c0dda2e5f2e41
Maximiliano Luci <maximiliano.luci@icloud.com>
2024-01-07 23:28:42 +0100

    add button component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,13 +5,15 @@
   >
     Toggle UI
   </div>
-  <div class="mx-auto m-3 p-3 h-auto">
+  <div class="grid grid-cols-5 gap-3 mx-auto m-3 p-3 h-auto">
     <card-component title="a">ickkcki2p2ai</card-component>
+    <button-component title="qwertyuiopasdfghjklñzxcvbnm" />
   </div>
 </template>
 
 <script setup lang="ts">
 import CardComponent from '../components/card/CardComponent.vue'
+import ButtonComponent from '../components/button/ButtonComponent.vue'
 
 let isDarkMode: boolean = false
 
