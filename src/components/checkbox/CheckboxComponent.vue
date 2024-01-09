f9c74c4f4f516681774c6b55e9061f386c712f0c
Maximiliano Luci <maximiliano.luci@icloud.com>
2024-01-09 15:08:08 +0100

    add HRs to homeview

--- src/components/checkbox/CheckboxComponent.vue
+++ src/components/checkbox/CheckboxComponent.vue
@@ -4,20 +4,18 @@
   >
     <div class="flex items-center h-5">
       <input
-        id="helper-checkbox"
-        aria-describedby="helper-checkbox-text"
+        :id="boxId"
+        :aria-describedby="helperId"
         type="checkbox"
         value=""
         class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
       />
     </div>
     <div class="ms-2 text-sm">
-      <label for="helper-checkbox" class="font-medium text-gray-900 dark:text-gray-300">{{
-        text
-      }}</label>
+      <label :for="boxId" class="font-medium text-gray-900 dark:text-gray-300">{{ text }}</label>
       <p
         v-if="subtitle !== null"
-        id="helper-checkbox-text"
+        :id="helperId"
         class="text-xs font-normal text-gray-500 dark:text-gray-300"
       >
         {{ subtitle }}
@@ -27,8 +25,14 @@
 </template>
 
 <script setup lang="ts">
-defineProps<{
+import { computed } from "vue";
+
+const props = defineProps<{
+  id?: string;
   text: string;
-  subtitle: string;
+  subtitle?: string;
 }>();
+
+const boxId = computed<string>(() => `${props.id}-menu`);
+const helperId = computed<string>(() => `${props.id}-description`);
 </script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,8 @@
 <template>
   <div class="p-3 h-auto">
-    <button-component
-      class="mb-3"
-      text="Toggle Dark/Light Mode"
-      color="dark"
-      @click="toggleUiMode"
-    />
-    <div class="grid grid-cols-2 mb-3">
+    <button-component class="" text="Toggle Dark/Light Mode" color="dark" @click="toggleUiMode" />
+    <hr class="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700" />
+    <div class="grid grid-cols-2">
       <card-component title="title" subtitle="subtitle" :hover="true"
         >i am inside your walls</card-component
       >
@@ -14,7 +10,8 @@
         >i am inside your walls</card-component
       >
     </div>
-    <div class="max-w-7xl mb-3">
+    <hr class="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700" />
+    <div class="max-w-7xl">
       <input-component
         type="text"
         labelValue="field test"
@@ -27,16 +24,21 @@
         placeholder="password field test"
         :requiredLabel="false"
       />
-      <button-group-component class="mt-3" :items="buttonItems" />
     </div>
-    <dropdown-component label="peepeepoopoo" :hideCaret="true" :items="dropdownItems" />
-    <div class="max-w-xl mt-3 mb-3">
-      <checkbox-component
-        text="Free shipping"
-        subtitle="For orders shipped from $25 in books or $29 in other categories"
-      />
+    <hr class="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700" />
+    <button-group-component :items="buttonItems" />
+    <hr class="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700" />
+    <div class="flex-none flex-row space-x-3 grow-0">
+      <dropdown-component id="dd1" label="peepeepoopoo" :hideCaret="true" :items="dropdownItems" />
+      <dropdown-component id="dd2" label="peepeepoopoo" :items="dropdownItems" />
     </div>
-    <div class="grid grid-rows-6 gap-3 max-w-xl mb-3">
+    <hr class="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700" />
+    <div class="flex-none flex-row space-y-3 grow-0">
+      <checkbox-component id="cb1" text="text" />
+      <checkbox-component id="cb2" text="text" subtitle="subtitle" />
+    </div>
+    <hr class="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700" />
+    <div class="grid grid-rows-6 gap-3 max-w-xl">
       <alert-component text="Change a few things up and try submitting again." icon="info-circle" />
       <alert-component text="Change a few things up and try submitting again." color="info" />
       <alert-component text="Change a few things up and try submitting again." color="danger" />
@@ -44,7 +46,8 @@
       <alert-component text="Change a few things up and try submitting again." color="warning" />
       <alert-component text="Change a few things up and try submitting again." color="dark" />
     </div>
-    <div class="flex-none flex-row space-x-3 grow-0 mb-3">
+    <hr class="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700" />
+    <div class="flex-none flex-row space-x-3 grow-0">
       <button-component text="transparent" color="transparent" size="xs" />
       <button-component text="primary" color="primary" size="base" />
       <button-component text="secondary" color="secondary" size="lg" />
@@ -53,7 +56,8 @@
       <button-component text="warning" color="warning" size="base" />
       <button-component text="info" color="info" size="xs" />
     </div>
-    <div class="flex-none flex-row space-x-3 grow-0 mb-3">
+    <hr class="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700" />
+    <div class="flex-none flex-row space-x-3 grow-0">
       <button-component icon="apple" text="transparent" color="transparent" size="xs" />
       <button-component icon="apple" text="primary" color="primary" size="base" />
       <button-component icon="apple" text="secondary" color="secondary" size="lg" />
@@ -62,7 +66,8 @@
       <button-component icon="apple" text="warning" color="warning" size="base" />
       <button-component icon="apple" text="info" color="info" size="xs" />
     </div>
-    <div class="flex-none flex-row space-y-3 grow-0 mb-3">
+    <hr class="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700" />
+    <div class="flex-none flex-row space-y-3 grow-0">
       <button-component text="transparent" color="transparent" size="xs" :full-width="true" />
       <button-component text="primary" color="primary" size="base" :full-width="true" />
       <button-component text="secondary" color="secondary" size="lg" :full-width="true" />
@@ -71,7 +76,8 @@
       <button-component text="warning" color="warning" size="base" :full-width="true" />
       <button-component text="info" color="info" size="xs" :full-width="true" />
     </div>
-    <div class="flex-none flex-row space-y-3 grow-0 mb-3">
+    <hr class="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700" />
+    <div class="flex-none flex-row space-y-3 grow-0">
       <button-component
         icon="apple"
         text="transparent"
